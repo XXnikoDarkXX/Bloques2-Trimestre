c6c4b4db55dfb557b5d35674f5e1cb3a82ec0c0c
XXnikoDarkXX <nicocheto1@gmail.com>
2020-02-25 21:03:06 +0100

    Fechas
    Cambiada clase de fecha hasta el ejercicio4

--- Bloque3/src/main/java/com/mycompany/bloque3/Bloque3.java
+++ Bloque3/src/main/java/com/mycompany/bloque3/Bloque3.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.bloque3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nicoc
@@ -15,19 +17,46 @@ public class Bloque3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
         // TODO code application logic here
-        Fecha actual=new Fecha();
-        actual.dia=23;
-        actual.mes=1;
-        actual.año=1987;
+        
+        
+        Fecha actual=new Fecha((byte)31,(byte)4, 222);
+        
+        
+        
         System.out.println(actual);
         System.out.println(actual.esValida(actual));
-       /* Fecha hola=new Fecha();
-        hola.año=1880;
-        hola.dia=12;
-        hola.mes=4;
-        System.out.println(actual);
-      */
+        //TO DO PREGUNTAR ESO DE MAYOR O IGUAL O MAYOR EN LA CLASE FECHA Y SI HAY QUE QUITAR EL TRUE
+       System.out.println("Parte 4");
+        System.out.println("Vamos a crear dos fechas");
+       Fecha poco1=new Fecha();
+       Fecha poco2=new Fecha();
+        System.out.println("Dime el dia de la fecha1");
+        byte dia=Byte.parseByte(sc.nextLine());
+        poco1.setDia(dia);
+        System.out.println("Dime el mes de la fecha1");
+        byte mes=Byte.parseByte(sc.nextLine());
+        poco1.setMes(mes);
+        System.out.println("Dime el año de la fecha1");
+        int año=Integer.parseInt(sc.nextLine());
+        poco1.setAño(año);
+        
+        System.out.println("Dime el dia de la fecha2");
+        dia=Byte.parseByte(sc.nextLine());
+        poco2.setDia(dia);
+        System.out.println("Dime el mes de la fecha2");
+        mes=Byte.parseByte(sc.nextLine());
+        poco2.setMes(mes);
+        System.out.println("Dime el año de la fecha2");
+        año=Integer.parseInt(sc.nextLine());
+        poco2.setAño(año);
+        
+        System.out.println("La fecha primera es "+poco1);
+        System.out.println("La fecha segunda es "+poco2);
+        System.out.println(actual.mayorQue(poco1,poco2));
+        
     }
-   
+
+
 }
--- Bloque3/src/main/java/com/mycompany/bloque3/Fecha.java
+++ Bloque3/src/main/java/com/mycompany/bloque3/Fecha.java
@@ -10,22 +10,43 @@ package com.mycompany.bloque3;
  * @author nicoc
  */
 public class Fecha {
-    int dia;
-    int mes;
-    int año;
-
+  private byte dia;//dia de la fecha
+   private byte mes;//mes de la fecha
+    private int año;//año de la fecha
+/**
+ * Constructor de fecha con todos sus parametros
+ * @param d dia de la fecha
+ * @param m mes de la fecha
+ * @param a año de la fecha
+ */
+  public Fecha(byte d,byte m,int a){
+      this.dia=d;
+      this.mes=m;
+      this.año=a;
+  }
+  
+  /**
+   * Constructor de animal vacio
+   */
+    public Fecha(){
+      
+  }
+  
+  
     @Override
     public String toString() {
         return "la fecha es: "+dia+"/" +mes+"/" + año;
+        
     }
    public boolean esValida(Fecha Actual){
-       if (this.mes==2&&dia>28) {
+      
+       if (this.getMes()==2&&dia>28) {
            return false;
        }//Buscar calendario 2019 y terminarlo debemos hacer que sea desde el 0 hasta el 31 para los días igual para los meses
        if ((mes==1||mes==3||mes==5||mes==7||mes==8||mes==10||mes==12)&&dia>31) {
            return false;
        }
-       if ((mes==4||mes==6||mes==9||mes==11) &&dia>31) {
+       if ((mes==4||mes==6||mes==9||mes==11) &&dia>=31) {
            return false;
        }
        if (mes<=0||mes>12) {
@@ -33,6 +54,76 @@ public class Fecha {
        }
        return true;
    }
+  
+ 
+   public boolean mayorQue(Fecha orig,Fecha prin){
+       
+       Fecha aux =new Fecha((byte)2,(byte)1,2);
+       //TO DO voy a crear otro parametro pues el fecha aux me da bastantes problemas
+       if ((orig.getDia()>prin.getDia()||orig.getMes()>prin.getMes())||orig.getAño()>prin.getAño()) {
+           System.out.println("La primera fecha es mayor");
+           return true;
+       }
+       //Preguntar porque no puedo ponerlo todo en un if                 
+       //En un parametro
+       /*if (aux.getDia()>orig.getDia()) {
+           if (aux.getMes()>orig.getMes()) {
+               if (aux.getAño()>orig.getAño()) {
+                   return false;
+               }
+           }
+  
+       }
+       
+       return true;*/
+       System.out.println("La segunda fecha es mayor");
+       return false;
+   }
+/**
+ * Getter de Dia
+ * @return el dia de la fecha
+ */
+    public int getDia() {
+        return dia;
+    }
+/**
+ * Getter del mes
+ * @return Devuelve el mes de la fecha
+ */
+    public int getMes() {
+        return mes;
+    }
+/**
+ * Getter del año
+ * @return nos devuelve el año
+ */
+    public int getAño() {
+        return año;
+    }
+/**
+ * Setter del dia
+ * @param dia dia al que queremos cambiar
+ */
+    public void setDia(byte dia) {
+        this.dia = dia;
+    }
+/**
+ * Setter del mes
+ * @param mes mes al cual queremos cambiar
+ */
+    public void setMes(byte mes) {
+        this.mes = mes;
+    }
+/**
+ * Setter del año
+ * @param año año al cual queremos cambiar
+ */
+    public void setAño(int año) {
+        this.año = año;
+    }
+   
+   
+   
 
    
 }
